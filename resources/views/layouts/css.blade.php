c574896f0442743997f2b249a85fd4c75af20e7d
Andri Sunardi <account@andrisunardi.com>
2025-02-21 11:40:45 +0700

    Update layouts css

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,4 +1,9 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/vendor.bundle.css') }}">
-<link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+{{-- <link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style-bitcoin.css') }}"> --}}
+{{-- <link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style-bluelight.css') }}"> --}}
+<link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style-green.css') }}">
+{{-- <link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style-orange.css') }}"> --}}
+{{-- <link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style-red.css') }}"> --}}
+{{-- <link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}"> --}}
