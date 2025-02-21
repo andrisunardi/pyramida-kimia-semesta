7fcf0a560ff3767e1af0f651181ad34ac4dff91d
Andri Sunardi <account@andrisunardi.com>
2025-02-21 13:33:31 +0700

    Update Providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
+    Andrisunardi\Library\LibraryServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    Barryvdh\Debugbar\ServiceProvider::class,
+    Barryvdh\DomPDF\ServiceProvider::class,
+    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+    Buglinjo\LaravelWebp\WebpServiceProvider::class,
+    Maatwebsite\Excel\ExcelServiceProvider::class,
+    PrettyRoutes\ServiceProvider::class,
+    SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+    Spatie\Menu\Laravel\MenuServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    ZanySoft\Zip\ZipServiceProvider::class,
 ];
