e7a38914a1e4c7ce213aaa792b630a3888a41306
Andri Sunardi <account@andrisunardi.com>
2025-02-21 12:38:48 +0700

    Update routes web - add locale

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('locale/{locale}', function ($locale) {
+    Session::put('locale', $locale);
+    App::setlocale(Session::get('locale'));
+
+    return redirect()->back();
+})->name('locale');
