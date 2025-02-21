0e50d4a1f01c59556cacca82ab6bc904a0a3135c
Andri Sunardi <account@andrisunardi.com>
2025-02-21 12:44:04 +0700

    Update Providers AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,33 @@
 
 namespace App\Providers;
 
+use Andrisunardi\Library\Utils;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        if (app()->isLocal()) {
+            app()->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Utils::boot();
+
+        LogViewer::auth(fn ($request) => optional($request->user())->hasRole('Super User') ?? false);
+
+        Gate::before(function (User $user) {
+            return $user->hasRole('Super User');
+        });
+
+        Gate::after(function (User $user) {
+            return $user->hasRole('Super User');
+        });
     }
 }
