2a1a147850a67bc4e0a87ecd2173cc8db8d68d64
Andri Sunardi <account@andrisunardi.com>
2025-02-21 13:45:08 +0700

    Update views layouts css

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,14 +1,4 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-<link
-    href="{{ asset('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i') }}"
-    rel="stylesheet">
-
-<link href="{{ asset('assets/aos/aos.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-
-<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/vendor.bundle.css') }}">
+<link id="style-css" rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
