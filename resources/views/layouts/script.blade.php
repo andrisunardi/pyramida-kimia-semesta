51901c4710ec085b1e922e23e89fa361e5974d02
Andri Sunardi <account@andrisunardi.com>
2025-02-22 21:07:11 +0700

    Update Script

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,16 +1,8 @@
 <script src="{{ asset('js/color-modes.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
 
-<script src="{{ asset('assets/purecounter/purecounter_vanilla.js') }}"></script>
-<script src="{{ asset('assets/aos/aos.js') }}"></script>
-<script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/glightbox/js/glightbox.min.js') }}"></script>
-<script src="{{ asset('assets/isotope-layout/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('assets/swiper/swiper-bundle.min.js') }}"></script>
-<script src="{{ asset('assets/waypoints/noframework.waypoints.js') }}"></script>
-<script src="{{ asset('assets/php-email-form/validate.js') }}"></script>
-
-<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/jquery.bundle.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
 
 @livewireChartsScripts
 <script src="{{ asset('https://cdn.jsdelivr.net/npm/apexcharts') }}"></script>
