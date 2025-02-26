6aab41fc4e1bf107f91a20a300b30335237706e7
Andri Sunardi <account@andrisunardi.com>
2025-02-26 22:04:14 +0800

    Add Language EN ID

---
+++ lang/en/index.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'copyright' => 'Copyright',
+    'created_and_designed_by' => 'Created and Designed by',
+    'contact_us_if_you_have_any_questions' => 'Contact Us If You Have Any Questions',
+    'all_right_reserved' => 'All Right Reserved',
+    'all_rights_reserved' => 'All Rights Reserved',
+    'data' => 'Data',
+];
