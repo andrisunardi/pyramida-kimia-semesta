4a0e2a7ffb9e5ca7a7143ef57681dc4baa66416d
Andri Sunardi <account@andrisunardi.com>
2025-02-28 01:01:54 +0800

    Update - Config - Livewire Alert

--- config/livewire-alert.php
+++ config/livewire-alert.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
- * For more details about the configuration, see:
- * https://sweetalert2.github.io/#configuration
- */
 return [
     'alert' => [
         'position' => 'top-end',
-        'timer' => 3000,
+        'timer' => 5000,
         'toast' => true,
-        'text' => null,
-        'showCancelButton' => false,
         'showConfirmButton' => false,
+        'timerProgressBar' => true,
     ],
     'confirm' => [
-        'icon' => 'warning',
         'position' => 'center',
+        'icon' => 'warning',
         'toast' => false,
         'timer' => null,
         'showConfirmButton' => true,
