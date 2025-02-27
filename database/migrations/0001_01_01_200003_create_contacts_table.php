9fbd13209e05c42b562e52200c0b1540d71c7f45
Andri Sunardi <account@andrisunardi.com>
2025-02-27 09:35:48 +0800

    Create Database Migrations Create Contacts Table

---
+++ database/migrations/0001_01_01_200003_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->nullable();
+            $table->string('company', 50)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->string('phone', 15)->nullable();
+            $table->string('subject', 100)->nullable();
+            $table->string('message', 1000)->nullable();
+            $table->boolean('is_active')->unsigned()->default(true);
+            $table->foreignIdFor(User::class, 'created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
