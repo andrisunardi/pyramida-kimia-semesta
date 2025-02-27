b567cf5ace02e44f9aa51499ff7da756b7f0fba2
Andri Sunardi <account@andrisunardi.com>
2025-02-27 09:46:23 +0800

    Add Language ZH

---
+++ lang/zh/index.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'copyright' => 'Hak Cipta',
+    'created_and_designed_by' => 'Dibuat dan Dirancang oleh',
+    'contact_us_if_you_have_any_questions' => 'Hubungi Kami Jika Anda Memiliki Pertanyaan',
+    'all_right_reserved' => 'Seluruh Hak Cipta Dilindungi',
+    'all_rights_reserved' => 'Seluruh Hak Cipta Dilindungi',
+    'data' => 'Data',
+];
