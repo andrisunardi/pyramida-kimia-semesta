48d6eca877898348186e3af6f8d47d7edb52f59f
Andri Sunardi <account@andrisunardi.com>
2025-02-27 14:54:22 +0800

    Create - Observers - Slider Observer

---
+++ app/Observers/SliderObserver.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Slider;
+use Illuminate\Support\Facades\Auth;
+
+class SliderObserver
+{
+    public function creating(Slider $slider)
+    {
+        $slider->created_by_id = Auth::slider()->id ?? null;
+        $slider->updated_by_id = Auth::slider()->id ?? null;
+    }
+
+    public function created(Slider $slider) {}
+
+    public function updating(Slider $slider)
+    {
+        $slider->updated_by_id = Auth::slider()->id ?? null;
+    }
+
+    public function updated(Slider $slider) {}
+
+    public function deleting(Slider $slider)
+    {
+        $slider->deleted_by_id = Auth::slider()->id ?? null;
+        $slider->save();
+    }
+
+    public function deleted(Slider $slider) {}
+
+    public function restoring(Slider $slider)
+    {
+        $slider->deleted_by_id = null;
+        $slider->save();
+    }
+
+    public function restored(Slider $slider) {}
+
+    public function forceDeleted(Slider $slider) {}
+}
