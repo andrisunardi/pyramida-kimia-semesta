c1b2c5daf915084727d37bf8a55a2db5cba4fca1
Andri Sunardi <account@andrisunardi.com>
2025-02-27 14:31:03 +0800

    Update Footer ]

--- resources/views/livewire/layouts/footer.blade.php
+++ resources/views/livewire/layouts/footer.blade.php
@@ -1,12 +1,10 @@
 <div>
-    <!-- Footer Widget-->
     <div class="footer-widget section-pad">
         <div class="container">
             <div class="row">
-
                 <div class="widget-row row">
+
                     <div class="footer-col col-md-5 col-sm-6 res-m-bttm">
-                        <!-- Each Widget -->
                         <div class="wgs wgs-footer wgs-menu">
                             <h5 class="wgs-title">Solutions</h5>
                             <div class="wgs-content">
@@ -26,10 +24,9 @@
                                 </ul>
                             </div>
                         </div>
-                        <!-- End Widget -->
                     </div>
+
                     <div class="footer-col col-md-2 col-sm-6 res-m-bttm">
-                        <!-- Each Widget -->
                         <div class="wgs wgs-footer wgs-menu">
                             <h5 class="wgs-title">Quick Links</h5>
                             <div class="wgs-content">
@@ -43,10 +40,9 @@
                                 </ul>
                             </div>
                         </div>
-                        <!-- End Widget -->
                     </div>
+
                     <div class="footer-col col-md-2 col-sm-6 res-m-bttm">
-                        <!-- Each Widget -->
                         <div class="wgs wgs-footer wgs-text">
                             <h5 class="wgs-title">Areas Serve</h5>
                             <div class="wgs-content">
@@ -60,31 +56,46 @@
                                 </ul>
                             </div>
                         </div>
-                        <!-- End Widget -->
                     </div>
+
                     <div class="footer-col col-md-3 col-sm-6">
-                        <!-- Each Widget -->
                         <div class="wgs wgs-footer">
                             <h5 class="wgs-title">Contact us</h5>
                             <div class="wgs-content">
-                                <p><strong>Industrial Company Name</strong><br>
-                                    1234 Sed spiciatis Road <br>
-                                    Atero eos, D58 8975, USA.</p>
-                                <p><span>Toll Free</span>: (1-800) 234 5678<br>
-                                    <span>Phone</span>: (123) 1234 5678
+                                <p>
+                                    <strong>{{ env('APP_NAME') }}</strong><br>
+                                    {{ env('CONTACT_ADDRESS') }}
+                                </p>
+                                <p>
+                                    <span>Phone</span>: {{ env('CONTACT_PHONE') }}<br>
+                                    <span>Email</span>: {{ env('CONTACT_EMAIL') }}
                                 </p>
                                 <ul class="social">
-                                    <li><a href="#"><em class="fa fa-facebook" aria-hidden="true"></em></a></li>
-                                    <li><a href="#"><em class="fa fa-twitter" aria-hidden="true"></em></a></li>
-                                    <li><a href="#"><em class="fa fa-linkedin" aria-hidden="true"></em></a></li>
+                                    <li>
+                                        <a draggable="false" href="{{ env('FACEBOOK_URL') }}" target="_blank">
+                                            <em class="fa fa-facebook" aria-hidden="true"></em>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a draggable="false" href="{{ env('TWITTER_URL') }}" target="_blank">
+                                            <em class="fab fa-twitter" aria-hidden="true"></em>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a draggable="false" href="{{ env('INSTAGRAM_URL') }}" target="_blank">
+                                            <em class="fa fa-instagram" aria-hidden="true"></em>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a draggable="false" href="{{ env('TIKTOK_URL') }}" target="_blank">
+                                            <em class="fa fa-tiktok" aria-hidden="true"></em>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
-                        <!-- End Widget -->
                     </div>
-
                 </div>
-
             </div>
         </div>
     </div>
