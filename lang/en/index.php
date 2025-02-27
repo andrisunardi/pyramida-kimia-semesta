78a410590739f8ea161073ae6123cc36d419e27d
Andri Sunardi <account@andrisunardi.com>
2025-02-27 14:25:38 +0800

    Update Lang

--- lang/en/index.php
+++ lang/en/index.php
@@ -7,6 +7,7 @@ return [
     'all_right_reserved' => 'All Right Reserved',
     'all_rights_reserved' => 'All Rights Reserved',
     'data' => 'Data',
+    'home' => 'Home',
     'menu' => 'Menu',
     'contact' => 'Contact',
 ];
