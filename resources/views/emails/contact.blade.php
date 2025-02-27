3ed5e931ea260c5c86178fdd8acc19ae2e7b7116
Andri Sunardi <account@andrisunardi.com>
2025-02-28 00:46:50 +0800

    Create - Views - Emails - Contact

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,13 @@
+<x-:mail::message>
+    CONTACT
+
+    Name : {{ $contact->name }}
+
+    Phone : {{ $contact->phone }}
+
+    Email : {{ $contact->email }}
+
+    Company : {{ $contact->company }}
+
+    Message : {{ $contact->message }}
+</x-mail::message>
