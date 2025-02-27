86cadbe3ee8e2f797bb50961c8816beebea90d80
Andri Sunardi <account@andrisunardi.com>
2025-02-28 00:54:31 +0800

    Create - Mail - Database Backup Daily Mail

---
+++ app/Mail/DatabaseBackupDailyMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class DatabaseBackupDailyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct() {}
+
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address(env('MAIL_FROM_ADDRESS'), env('APP_NAME')),
+            subject: 'Database Backup Daily - '.now()->isoFormat('dddd, DD MMMM YYYY'),
+        );
+    }
+
+    public function content()
+    {
+        return new Content(
+            markdown: 'emails.database-backup-daily',
+        );
+    }
+
+    public function attachments()
+    {
+        $date = now()->toDateString();
+
+        return [
+            // Attachment::fromPath(storage_path("app/private/database/{$date}.sql").'/app/database/'.$date.'.sql'),
+            Attachment::fromStorage("database/{$date}.sql")
+                ->as("database/{$date}.sql")
+                ->withMime('application/gzip'),
+        ];
+    }
+}
