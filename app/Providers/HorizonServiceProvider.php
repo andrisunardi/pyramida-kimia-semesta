a66817a6eded2baf60372c2dc8fecedaa4cff033
Andri Sunardi <account@andrisunardi.com>
2025-03-17 00:27:44 +0800

    Update - Providers - Horizon Service Provider

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,35 +2,26 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         parent::boot();
 
-        // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
-        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+        Horizon::routeSmsNotificationsTo(env('SLACK_SMS'));
+        Horizon::routeMailNotificationsTo(env('SLACK_MAIL'));
+        Horizon::routeSlackNotificationsTo(env('LOG_SLACK_WEBHOOK_URL'), '#'.env('SLACK_CHANNEL'));
     }
 
-    /**
-     * Register the Horizon gate.
-     *
-     * This gate determines who can access Horizon in non-local environments.
-     */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+        Gate::define('viewHorizon', function (User $user) {
+            return $user->hasRole('Super User');
         });
     }
 }
