dd1419e69071b11b4c7429dd8943e76fc21363d5
Andri Sunardi <account@andrisunardi.com>
2025-03-17 00:32:53 +0800

    Create - Database - Migrations - Create Testimonies Table

---
+++ database/migrations/0001_01_01_200003_create_testimonies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('testimonies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('company', 50);
+            $table->string('message', 1000);
+            $table->boolean('is_active')->unsigned()->default(true);
+            $table->foreignIdFor(User::class, 'created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonies');
+    }
+};
