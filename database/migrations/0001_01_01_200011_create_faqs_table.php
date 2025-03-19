4ac58a6fc9743bb39f8ed7ffd299c03dabdfa2f6
Andri Sunardi <account@andrisunardi.com>
2025-03-19 22:36:14 +0800

    Database - Migrations - Create Faqs Table

---
+++ database/migrations/0001_01_01_200011_create_faqs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->id();
+            $table->string('question', 50)->unique();
+            $table->string('question_id', 50)->unique();
+            $table->string('question_zh', 50)->unique();
+            $table->text('answer')->nullable();
+            $table->text('answer_id')->nullable();
+            $table->text('answer_zh')->nullable();
+            $table->boolean('is_active')->unsigned()->default(true);
+            $table->foreignIdFor(User::class, 'created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('faqs');
+    }
+};
