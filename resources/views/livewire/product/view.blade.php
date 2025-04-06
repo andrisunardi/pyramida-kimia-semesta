205854d378bfd07f2101104b207952026fbd8a27
Andri Sunardi <account@andrisunardi.com>
2025-04-06 15:29:43 +0800

    Update Product View Page Asset File Coa And Msds

--- resources/views/livewire/product/view.blade.php
+++ resources/views/livewire/product/view.blade.php
@@ -25,17 +25,48 @@
 
                         <div class="feature-imagebox border download-action">
                             <div class="block">
-                                <h3>Download PDF Datasheet</h3>
-                                {{-- <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p> --}}
-                                <a href="#" target="_blank" class="btn">
-                                    {{ trans('index.download') }} COA
-                                    <i class="fa fa-file-pdf-o"></i>
-                                </a>
+                                <h3>
+                                    @if (App::isLocale('en'))
+                                        Download PDF Datasheet
+                                    @endif
+                                    @if (App::isLocale('id'))
+                                        Unduh Lembar Data PDF
+                                    @endif
+                                    @if (App::isLocale('zh'))
+                                        下载 PDF 数据表
+                                    @endif
+                                </h3>
+                                <p>
+                                    @if (App::isLocale('en'))
+                                        Access essential documents such as Product Datasheets, Certificates of Analysis
+                                        (COA), and Material Safety Data Sheets (MSDS) in PDF format. Click below to
+                                        download and stay informed about the quality, safety, and specifications of our
+                                        chemical products.
+                                    @endif
+                                    @if (App::isLocale('id'))
+                                        Akses dokumen penting seperti Lembar Data Produk, Sertifikat Analisis (COA), dan
+                                        Lembar Data Keselamatan Material (MSDS) dalam format PDF. Klik di bawah ini
+                                        untuk mengunduh dan tetap mendapatkan informasi tentang kualitas, keamanan, dan
+                                        spesifikasi produk kimia kami.
+                                    @endif
+                                    @if (App::isLocale('zh'))
+                                        以 PDF 格式获取产品数据表、分析证书 (COA) 和材料安全数据表 (MSDS) 等重要文件。点击下方下载并随时了解我们化学产品的质量、安全性和规格。
+                                    @endif
+                                </p>
 
-                                <a href="#" target="_blank" class="btn">
-                                    {{ trans('index.download') }} MSDS
-                                    <i class="fa fa-file-pdf-o"></i>
-                                </a>
+                                @if ($product->checkFileCoa())
+                                    <a draggable="false" href="{{ $product->assetFileCoa() }}" class="btn" download>
+                                        {{ trans('index.download') }} COA
+                                        <i class="fa fa-file-pdf-o"></i>
+                                    </a>
+                                @endif
+
+                                @if ($product->checkFileMsds())
+                                    <a draggable="false" href="{{ $product->assetFileMsds() }}" class="btn" download>
+                                        {{ trans('index.download') }} MSDS
+                                        <i class="fa fa-file-pdf-o"></i>
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </div>
