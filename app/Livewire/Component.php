c882adfcf452b377744b6a56137aeadb9edfce58
Andri Sunardi <account@andrisunardi.com>
2025-04-07 21:09:00 +0800

    Update - Component

--- app/Livewire/Component.php
+++ app/Livewire/Component.php
@@ -12,4 +12,31 @@ class Component extends LivewireComponent
     use LivewireAlert;
     use WithFileUploads;
     use WithPagination;
+
+    public function getBgClasses()
+    {
+        return collect([
+            'bg-primary',
+            'bg-success',
+            'bg-warning',
+            'bg-danger',
+            'bg-info',
+            'bg-secondary',
+        ]);
+    }
+
+    public function resetFile()
+    {
+        $this->reset(['file']);
+    }
+
+    public function resetImage()
+    {
+        $this->reset(['image']);
+    }
+
+    public function resetVideo()
+    {
+        $this->reset(['video']);
+    }
 }
