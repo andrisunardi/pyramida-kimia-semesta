2a8a309926aa38313d4b685e8a646d9206960a8c
Andri Sunardi <account@andrisunardi.com>
2025-04-12 19:36:26 +0800

    Update Search Menu And Sidebar

--- resources/views/livewire/cms/layouts/search-menu.blade.php
+++ resources/views/livewire/cms/layouts/search-menu.blade.php
@@ -3,6 +3,51 @@
         <option value="cms.index" {{ Route::is('cms.index') ? 'selected' : null }}>
             {{ trans('index.home') }}
         </option>
+
+        @foreach ($menus as $menu)
+            @role($menu['roles'])
+                @can($menu['permissions'])
+                    @if ($menu['subMenus'])
+                        <optgroup label="{{ Utils::translate($menu['name']) }}">
+                            @foreach ($menu['subMenus'] as $subMenu)
+                                @role($subMenu['roles'])
+                                    @can($subMenu['permissions'])
+                                        <option value="{{ $subMenu['route'] }}"
+                                            {{ Route::is($subMenu['route']) || Route::is(Str::before($subMenu['route'], '.index') . '.*') ? 'selected' : null }}>
+                                            {{ Utils::translate($subMenu['name']) }}
+                                        </option>
+                                    @endcan
+                                @endrole
+                            @endforeach
+                        </optgroup>
+                    @else
+                        <option value="{{ $menu['route'] }}"
+                            {{ Route::is($menu['route']) || Route::is(Str::before($menu['route'], '.index') . '.*') ? 'selected' : null }}>
+                            {{ Utils::translate($menu['name']) }}
+                        </option>
+                    @endif
+                @endcan
+            @endrole
+        @endforeach
+
+        <optgroup label="{{ trans('index.my_profile') }}">
+            <option value="cms.profile.index" {{ Route::is('cms.profile.index') ? 'selected' : null }}>
+                {{ trans('index.profile') }}
+            </option>
+            <option value="cms.profile.activity-log" {{ Route::is('cms.profile.activity-log') ? 'selected' : null }}>
+                {{ trans('index.activity_log') }}
+            </option>
+            <option value="cms.profile.edit-profile" {{ Route::is('cms.profile.edit-profile') ? 'selected' : null }}>
+                {{ trans('index.edit_profile') }}
+            </option>
+            <option value="cms.profile.change-password"
+                {{ Route::is('cms.profile.change-password') ? 'selected' : null }}>
+                {{ trans('index.change_password') }}
+            </option>
+            <option value="cms.logout">
+                {{ trans('index.logout') }}
+            </option>
+        </optgroup>
     </select>
 
     <button class="btn btn-light border-0 rounded-0" type="submit" wire:click="submit">
--- resources/views/livewire/cms/layouts/sidebar.blade.php
+++ resources/views/livewire/cms/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside id="sidebar"
-    class="collapse collapse-horizontal {{ !$isMobile ? "show" : null }} col-sm-4 col-md-4 col-lg-3 col-xl-2 vh-100 p-0 m-0 overflow-auto">
+    class="collapse collapse-horizontal {{ !$isMobile ? 'show' : null }} col-sm-4 col-md-4 col-lg-3 col-xl-2 vh-100 p-0 m-0 overflow-auto">
     <ul class="list-group mt-5 pt-3">
         <li>
             @livewire('c-m-s.layouts.search-menu')
@@ -14,6 +14,129 @@
                     <span>{{ trans('index.home') }}</span>
                 </span>
             </a>
+        </li>
+
+        @foreach ($menus as $menu)
+            @role($menu['roles'])
+                @can([$menu['permissions']])
+                    @if ($menu['subMenus'])
+                        <li>
+                            <a draggable="false"
+                                class="list-group-item list-group-item-action d-flex justify-content-between align-items-center {{ Route::is($menu['route']) || Route::is(Str::before($menu['route'], '.index') . '.*') ? 'active' : null }}"
+                                href="#{{ Str::slug($menu['name']) }}-sidebar" data-bs-toggle="collapse">
+                                <span>
+                                    <span class="{{ $menu['icon'] }} fa-fw me-1"></span>
+                                    <span>{{ Utils::translate($menu['name']) }}</span>
+                                </span>
+                                <span class="fas fa-caret-down"></span>
+                            </a>
+
+                            <ul class="list-group list-group-flush collapse
+                                {{ Route::is($menu['route']) || Route::is(Str::before($menu['route'], '.index') . '.*') ? 'show' : null }}"
+                                id="{{ Str::slug($menu['name']) }}-sidebar" data-bs-parent="#sidebar">
+                                @foreach ($menu['subMenus'] as $subMenu)
+                                    @role($subMenu['roles'])
+                                        @can($subMenu['permissions'])
+                                            @if ($subMenu['sidebar'])
+                                                <li>
+                                                    <a draggable="false"
+                                                        class="list-group-item d-flex justify-content-between align-items-center
+                                                    {{ Route::is($subMenu['route']) || Route::is(Str::before($subMenu['route'], '.index') . '.*') ? 'active' : null }}"
+                                                        href="{{ route($subMenu['route']) }}" wire:navigate>
+                                                        <span class="ms-4">
+                                                            <span class="{{ $subMenu['icon'] }} fa-fw me-1"></span>
+                                                            <span>{{ Utils::translate($subMenu['name']) }}</span>
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                            @endif
+                                        @endcan
+                                    @endrole
+                                @endforeach
+                            </ul>
+                        </li>
+                    @else
+                        @if ($menu['sidebar'])
+                            <li>
+                                <a draggable="false"
+                                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center
+                                    {{ Route::is($menu['route']) || Route::is(Str::before($menu['route'], '.index') . '.*') ? 'active' : null }}"
+                                    href="{{ route($menu['route']) }}" wire:navigate>
+                                    <span>
+                                        <span class="{{ $menu['icon'] }} fa-fw me-1"></span>
+                                        <span>{{ Utils::translate($menu['name']) }}</span>
+                                    </span>
+                                </a>
+                            </li>
+                        @endif
+                    @endif
+                @endcan
+            @endrole
+        @endforeach
+
+        <li>
+            <a draggable="false"
+                class="list-group-item list-group-item-action d-flex justify-content-between align-items-center {{ Route::is('cms.profile.*') ? 'active' : null }}"
+                href="#profile-sidebar" data-bs-toggle="collapse">
+                <span>
+                    <span class="fas fa-user-circle fa-fw me-1"></span>
+                    <span>{{ trans('index.my_profile') }}</span>
+                </span>
+                <span class="fas fa-caret-down"></span>
+            </a>
+
+            <ul class="list-group list-group-flush collapse {{ Route::is('cms.profile.*') ? 'show' : null }}"
+                id="profile-sidebar" data-bs-parent="#sidebar">
+                <li>
+                    <a draggable="false"
+                        class="list-group-item d-flex justify-content-between align-items-center {{ Route::is('cms.profile.index') ? 'active' : null }}"
+                        href="{{ route('cms.profile.index') }}" wire:navigate>
+                        <span class="ms-4">
+                            <span class="fas fa-user fa-fw me-1"></span>
+                            <span>{{ trans('index.profile') }}</span>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a draggable="false"
+                        class="list-group-item d-flex justify-content-between align-items-center {{ Route::is('cms.profile.activity-log') ? 'active' : null }}"
+                        href="{{ route('cms.profile.activity-log') }}" wire:navigate>
+                        <span class="ms-4">
+                            <span class="fas fa-user-clock fa-fw me-1"></span>
+                            <span>{{ trans('index.activity_log') }}</span>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a draggable="false"
+                        class="list-group-item d-flex justify-content-between align-items-center {{ Route::is('cms.profile.edit-profile') ? 'active' : null }}"
+                        href="{{ route('cms.profile.edit-profile') }}" wire:navigate>
+                        <span class="ms-4">
+                            <span class="fas fa-user-edit fa-fw me-1"></span>
+                            <span>{{ trans('index.edit_profile') }}</span>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a draggable="false"
+                        class="list-group-item d-flex justify-content-between align-items-center {{ Route::is('cms.profile.change-password') ? 'active' : null }}"
+                        href="{{ route('cms.profile.change-password') }}" wire:navigate>
+                        <span class="ms-4">
+                            <span class="fas fa-user-lock fa-fw me-1"></span>
+                            <span>{{ trans('index.change_password') }}</span>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a draggable="false" class="list-group-item d-flex justify-content-between align-items-center"
+                        href="{{ route('cms.logout') }}" wire:navigate>
+                        <span class="ms-4">
+                            <span class="fas fa-sign-out-alt fa-fw me-1"></span>
+                            <span>{{ trans('index.logout') }}</span>
+                        </span>
+                    </a>
+                </li>
+            </ul>
         </li>
     </ul>
 </aside>
