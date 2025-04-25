56e37c9242e65fd35b7ffee27a269a27ad0b830e
Andri Sunardi <account@andrisunardi.com>
2025-04-25 14:42:24 +0800

    Update - Seeders - Permission Seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -3,8 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
-    public function run(): void {}
+    public function run(): void
+    {
+        Permission::create(['name' => 'contact', 'guard_name' => 'web'])->assignRole('Admin');
+        Permission::create(['name' => 'contact.detail', 'guard_name' => 'web'])->assignRole('Admin');
+        Permission::create(['name' => 'contact.export', 'guard_name' => 'web'])->assignRole('Admin');
+    }
 }
