54241edeb36a6f92ba4338449f0d8edf683d76c0
Andri Sunardi <account@andrisunardi.com>
2025-04-25 14:41:03 +0800

    Update - Services - Role Seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -9,14 +9,7 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        Role::create([
-            'name' => 'Super User',
-            'guard_name' => 'web',
-        ]);
-
-        Role::create([
-            'name' => 'Admin',
-            'guard_name' => 'web',
-        ]);
+        Role::create(['name' => 'Super User','guard_name' => 'web']);
+        Role::create(['name' => 'Admin','guard_name' => 'web']);
     }
 }
