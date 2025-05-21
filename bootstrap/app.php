0c1005a7625fef945b9f94cb0eeaba4e94acef99
Andri Sunardi <account@andrisunardi.com>
2025-05-21 08:54:37 +0800

    Update - Boostrap - App - Import Str

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
