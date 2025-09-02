ddfb00c66cbb0267ceacc5b8f700b30b9783082d
Andri Sunardi <account@andrisunardi.com>
2025-09-02 18:26:39 +0800

    Update - Images - Slider - And - Banner

--- resources/views/livewire/banner.blade.php
+++ resources/views/livewire/banner.blade.php
@@ -2,7 +2,11 @@
     <div class="container">
         <div class="content row has-bg-image">
             <div class="banner-text">
-                <h1 class="page-title">{{ $title }}</h1>
+                @if (!Route::is('article.*') && !Route::is('contact'))
+                    <h1 class="page-title"
+                        @if (Route::is('career') || Route::is('faq')) style="color: #fff; -webkit-text-stroke: 1px #000;" @endif>
+                        {{ $title }}</h1>
+                @endif
             </div>
             <div class="imagebg bg-image-loaded" style="background-image: url({{ $image }})">
                 <x-components::image :src="$image" :alt="$title . ' - ' . env('APP_TITLE')" />
